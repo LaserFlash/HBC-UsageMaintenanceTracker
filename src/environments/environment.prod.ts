0b736a21ca7df9e1a3ae03ff0bc681927991ff8e
Bryn Bennett <bryn.t.bennett@gmail.com>
2019-09-24 17:04:57 +1200

    Setup with HBC-Boats firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,12 +1,13 @@
 export const environment = {
   production: true,
-  version: '0.0.0',
+  version: "0.0.0",
   firebase: {
-    apiKey: 'AIzaSyCZzR8zwpm6mg4lL5woVJs7UXdzosAUtzA',
-    authDomain: 'npyc-usagemaintenancetracker.firebaseapp.com',
-    databaseURL: 'https://npyc-usagemaintenancetracker.firebaseio.com',
-    projectId: 'npyc-usagemaintenancetracker',
-    storageBucket: 'npyc-usagemaintenancetracker.appspot.com',
-    messagingSenderId: '675501230461'
+    apiKey: "AIzaSyAMcLPFzdpH97ctswmfT0KoKQSCSIGGmbw",
+    authDomain: "hbc-boats.firebaseapp.com",
+    databaseURL: "https://hbc-boats.firebaseio.com",
+    projectId: "hbc-boats",
+    storageBucket: "",
+    messagingSenderId: "764529683827",
+    appId: "1:764529683827:web:23146a200dec66d2292906"
   }
 };
